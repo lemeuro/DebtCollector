ecba40ee939579464f1764d2e070a6b25a132a67
Lem Euro <mr.lemeuro@gmail.com>
2022-07-11 18:06:27 +0300

    Initial Commit

---
+++ DebtCollector/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DebtCollector
+//
+//  Created by Lem Euro on 11.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DebtCollector/DebtCollectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebtCollectorApp.swift
+//  DebtCollector
+//
+//  Created by Lem Euro on 11.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct DebtCollectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
